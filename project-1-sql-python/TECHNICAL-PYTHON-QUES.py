1bfaac90c21be4c0fff99ebae57b7961b292f2b0
sayyamdoshii <sayyamdoshi06@outlook.com>
2026-07-30 09:53:13 -0400

    Update TECHNICAL-PYTHON-QUES.py

--- project-1-sql-python/TECHNICAL-PYTHON-QUES.py
+++ project-1-sql-python/TECHNICAL-PYTHON-QUES.py
@@ -1,1 +1,17 @@
-import pandas as py
+import pandas as pd
+def flag_leakage_orders(orders_df, items_df, payments_df, refunds_df, tolerance=1.0)
+#going through each order at one time
+for order_id in orders_df["order_id"]:
+#order worth, total value
+order_items = items_df[items_df["order_id"] == order_id]
+order_value = (order_items["qty"] * order_items["unit_price"] * (1 - order_items["discount_pct"])).sum()
+#order completed payments
+order_payments = payments_df[
+(payments_df["order_id"] == order_id) 
+& (payments_df["payment_status"] == "completed") ]
+total_paid = order_payments["payment_amount"].sum()
+
+
+
+
+
